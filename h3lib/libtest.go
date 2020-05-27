a4e20f8ce1446cc0cbccc7e1904fb0e4fbd9d141
Escapethemoi <escapethemoi@gmail.com>
2020-05-27 18:22:23 +0300

    Added more or less finished libtest

--- h3lib/libtest.go
+++ h3lib/libtest.go
@@ -1,7 +1,18 @@
 package main
 
 import "fmt"
+import "bufio"
+import "os"
+
+
+//echo simulator: takes text input, loops it
 
 func main() {
-	fmt.Println("hei")
+	fmt.Println("Enter text for echo:")
+	reader := bufio.NewReader(os.Stdin)
+	byteOption, _, _ := reader.ReadLine()
+	echo := string(byteOption)
+	for i := 0; i < 3; i++ {
+	fmt.Println(echo)
+	}
 }
